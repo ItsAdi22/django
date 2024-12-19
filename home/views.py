d671bee358d59d682e16eb6751443e64b68393d5
Aditya-Laptop <adityamondkar111@gmail.com>
2024-12-20 02:09:13 +0530

    somethin not right

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is home page")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("This is about page")
