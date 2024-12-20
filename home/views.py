d5b6d6d8312fb18f2ed00f6dea4cf11a2e1c6c63
Aditya Mondkar <adityamondkar111@gmail.com>
2024-12-20 17:24:31 +0530

    all changes done

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
         "name":"Aditya",
         "age":22
     }
+    messages.success(request,"this is a test message")
     return render(request, "index.html",context)
 
 def about(request):
@@ -22,5 +24,6 @@ def contact(request):
 
         contact = Contact(name=name, email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request, "Form submitted successfully")
 
     return render(request, "contact.html")
