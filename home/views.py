f1fa15f453bc5b6580e2214d6014fbd0afa760fa
Aditya Mondkar <adityamondkar111@gmail.com>
2024-12-20 16:50:04 +0530

    functional contact page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 
 def index(request):
@@ -13,4 +14,13 @@ def about(request):
     return render(request, "about.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        desc = request.POST.get("desc")
+
+        contact = Contact(name=name, email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+
     return render(request, "contact.html")
